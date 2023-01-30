d9a72d2deb83cd662ef16093bb8ff17314534b63
yashchandel0007 <yashchandel007@outlook.com>
2023-01-30 21:58:50 +0530

    Helper Class Added, code cleanup

--- Invoice.Generator.Api/CommandHandler/CreateUserCommandHandler.cs
+++ Invoice.Generator.Api/CommandHandler/CreateUserCommandHandler.cs
@@ -3,20 +3,13 @@ using Invoice.Generator.Models.DB;
 using Invoice.Generator.Models.RequestResponse;
 using MediatR;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Invoice.Generator.Models;
-using System.Security.Cryptography;
 using Couchbase;
-using Couchbase.Management.Query;
 using Couchbase.KeyValue;
 using Couchbase.Extensions.DependencyInjection;
 using System.Linq;
 using Couchbase.Transactions;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Couchbase.Transactions.Config;
 using Couchbase.Transactions.Error;
 
@@ -39,35 +32,21 @@ namespace Invoice.Generator.Api.CommandHandler
             userDetails.username = command.Request.Email;
             userDetails._type = Constants.DocumentType.User;
             userDetails.iterations = 10000;
-            byte[] salt = GenerateSalt();
+            var salt = HelperClass.GenerateSalt();
             userDetails.salt = Convert.ToBase64String(salt);
-            userDetails.passwordHash = Convert.ToBase64String(GetPasswordHash(command.Request.Password, salt, userDetails.iterations));
+            userDetails.passwordHash = Convert.ToBase64String(HelperClass.GetPasswordHash(command.Request.Password, salt, userDetails.iterations));
             userDetails.userID = "user::"+Guid.NewGuid();
 
             return PushToCouchbaseAsync(userDetails, command.Request).Result;
-        }
-
-        private byte[] GetPasswordHash(string password, byte[] salt, int iterations)
-        {
-            Rfc2898DeriveBytes hash = new Rfc2898DeriveBytes(password, salt, iterations);
-            return hash.GetBytes(24);
-        }
-
-        private byte[] GenerateSalt()
-        {
-            byte[] salt = new byte[10];
-            using (RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider())
-            {
-                // Fill the array with a random value.
-                rngCsp.GetBytes(salt);
-            }
-            return salt;
         }
 
         private async Task<CreateUserResponse> PushToCouchbaseAsync(UserDetails userDetails, CreateUserRequest request)
         {
             var bucket = _bucketProvider.GetBucketAsync("IG_Config_LogIn").Result;
             var collection = bucket.DefaultCollection();
+            var cluster = _clusterProvider.GetClusterAsync().Result;
+
+
             var newUserID = GetUserId(collection, userDetails.username);
             if (newUserID != null)
                 return new CreateUserResponse();
@@ -77,65 +56,17 @@ namespace Invoice.Generator.Api.CommandHandler
             
             var response = new CreateUserResponse();
 
-
-
-            //try
-            //{
-            //    await transactions.RunAsync(async (ctx) =>
-            //    {
-            //        //insert log in information
-            //        //var _userTypeDoc = await ctx.InsertAsync(collection , "" + Guid.NewGuid(), userDetails).ConfigureAwait(false);
-            //        await ctx.QueryAsync<object>(
-            //            statement: $"INSERT INTO `IG_Config_LogIn` VALUES ('{"" + Guid.NewGuid()}',{userDetails})",
-            //            TransactionQueryConfigBuilder.Create()
-            //              );
-
-
-            //        //insert config data
-            //        //var _userConfigTypeDoc = await ctx.InsertAsync(collection, "" + Guid.NewGuid(), userConfigDetails).ConfigureAwait(false);
-
-            //        var _userConfigTypeDoc = await ctx.QueryAsync<object>(
-            //            statement: $"INSERT INTO `IG_Config_LogIn` VALUES ('{"" + Guid.NewGuid()}',{userConfigDetails})",
-            //            scope: _default);
-
-            //        //await ctx.CommitAsync().ConfigureAwait(false);
-            //    });
-
-            //}
-            //catch (TransactionCommitAmbiguousException e)
-            //{
-            //    Console.WriteLine("Transaction possibly committed");
-            //    Console.WriteLine(e);
-            //}
-            //catch (TransactionFailedException e)
-            //{
-            //    foreach (var logLine in e.Result.Logs)
-            //    {
-            //        Console.Error.WriteLine(logLine);
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    response.error = ex.Message;
-            //} 
-
-            var options = new ClusterOptions().WithCredentials("Administrator", "Administrator");
-            var cluster = await Cluster.ConnectAsync("couchbase://localhost", options).ConfigureAwait(false);
-            var bucket1 = await cluster.BucketAsync("IG_Config_LogIn").ConfigureAwait(false);
-            var collection1 = await bucket.ScopeAsync("_default").Result.CollectionAsync("_default").ConfigureAwait(false);
-
-            // Create the single Transactions object
             var transactions = Transactions.Create(cluster, TransactionConfigBuilder.Create().DurabilityLevel(DurabilityLevel.None)
-        .Build());
+            .Build());
+
             try
             {
                 var result = await transactions.RunAsync(async ctx =>
                 {
-                    var _userTypeDoc = await ctx.InsertAsync(collection1, "" + Guid.NewGuid(), userDetails).ConfigureAwait(false);
+                    var _userTypeDoc = await ctx.InsertAsync(collection, "" + Guid.NewGuid(), userDetails).ConfigureAwait(false);
                     
                     var _userConfigTypeDoc = await ctx.InsertAsync(collection, "" + Guid.NewGuid(), userConfigDetails).ConfigureAwait(false);
-                    
-                    Console.WriteLine("inside transaction");
+                  
                     await ctx.CommitAsync().ConfigureAwait(false);
                 }).ConfigureAwait(false);
             }
@@ -150,8 +81,6 @@ namespace Invoice.Generator.Api.CommandHandler
                 }
 
             }
-            
-
 
             response.emailAlreadyExists = false;
             response.isUserCreated = true;
@@ -173,11 +102,6 @@ namespace Invoice.Generator.Api.CommandHandler
             if (temp?.FirstOrDefault()?.userID != null)
                 return temp?.FirstOrDefault()?.userID;
             return null;
-        }
-
-        public class Tran
-        {
-            public string txid;
         }
         
 
---
+++ Invoice.Generator.Api/CommandHandler/HelperClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Invoice.Generator.Api.CommandHandler
+{
+    public static class HelperClass 
+    {
+        public static byte[] GetPasswordHash(string password, byte[] salt, int iterations)
+        {
+            Rfc2898DeriveBytes hash = new Rfc2898DeriveBytes(password, salt, iterations);
+            return hash.GetBytes(24);
+        }
+
+        public static byte[] GenerateSalt()
+        {
+            byte[] salt = new byte[10];
+            using (RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider())
+            {
+                // Fill the array with a random value.
+                rngCsp.GetBytes(salt);
+            }
+            return salt;
+        }
+    }
+}
